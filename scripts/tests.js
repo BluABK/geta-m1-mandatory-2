d8d124c609143afc8b460a81183e9e9c91758232
BluABK <bluabk@bluabk.net>
2021-04-24 00:08:55 +0200

    Baseline code.

---
+++ scripts/tests.js
@@ -0,0 +1,23 @@
+QUnit.test("FIXME: Select bar (selected and another).", function (assert) {
+    let FIXME = null;
+
+    assert.notEqual(FIXME, null);
+});
+
+QUnit.test("FIXME: Delete bar.", function (assert) {
+    let FIXME = null;
+
+    assert.notEqual(FIXME, null);
+});
+
+QUnit.test("FIXME: Modify bar.", function (assert) {
+    let FIXME = null;
+
+    assert.notEqual(FIXME, null);
+});
+
+QUnit.test("FIXME: Add bar.", function (assert) {
+    let FIXME = null;
+
+    assert.notEqual(FIXME, null);
+});
