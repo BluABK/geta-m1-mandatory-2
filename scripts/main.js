f94c347fcf0adcff82c7abb85387d67acfec71cb
BluABK <bluabk@bluabk.net>
2021-04-24 00:19:16 +0200

    Reformat and customise baseline to own pref

--- scripts/main.js
+++ scripts/main.js
@@ -6,20 +6,22 @@ let inputValue; // Variabel for hva som er skrevet i input-feltet
 // View
 function show() {
     let svgInnerHtml = '';
+
     for (let i = 0; i < numbers.length; i++) {
         svgInnerHtml += createBar(numbers[i], i + 1);
     }
+
     document.getElementById('content').innerHTML = `
-            <svg id="chart" width="500" viewBox="0 0 80 60">
-                ${svgInnerHtml}
-            </svg><br/>
-            Valgt stolpe: <i>ingen</i>
-            <br />
-            Verdi:
-            <input type="number" min="1" max="10" oninput="inputValue = this.value" />
-            <button>Legg til stolpe</button>
-            <button disabled>Endre valgt stolpe</button><br />
-            <button disabled>Fjerne valgt stolpe</button>
+                <svg id="chart" width="500" viewBox="0 0 80 60">
+                    ${svgInnerHtml}
+                </svg><br />
+                Valgt stolpe: <i>ingen</i>
+                <br />
+                Verdi:
+                <input type="number" min="1" max="10" oninput="inputValue = this.value" />
+                <button>Legg til stolpe</button>
+                <button disabled>Endre valgt stolpe</button><br />
+                <button disabled>Fjerne valgt stolpe</button>
             `;
 }
 
@@ -30,14 +32,17 @@ function createBar(number, barNo) {
     let height = number * 10;
     let y = 60 - height;
     let color = calcColor(1, 10, barNo);
+
     return `<rect width="${width}" height="${height}"
-                            x="${x}" y="${y}" fill="${color}"></rect>`;
+                  x="${x}" y="${y}" fill="${color}">
+            </rect>`;
 }
 
 function calcColor(min, max, val) {
-    var minHue = 240, maxHue = 0;
-    var curPercent = (val - min) / (max - min);
-    var colString = "hsl(" + ((curPercent * (maxHue - minHue)) + minHue) + ",100%,50%)";
+    let minHue = 240, maxHue = 0;
+    let curPercent = (val - min) / (max - min);
+    let colString = "hsl(" + ((curPercent * (maxHue - minHue)) + minHue) + ",100%,50%)"; //FIXME: Redundant vardecl?
+
     return colString;
 }
 
