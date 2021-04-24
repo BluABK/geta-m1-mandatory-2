2fd87ada09801505744ec219b0189b59797cf336
BluABK <bluabk@bluabk.net>
2021-04-24 15:26:53 +0200

    Bars now selectable, selected bar has border, text updates and buttons toggle.

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,54 @@
-// Model
+// -=MODEL=-
+// Array of numbers to create bar graphs from.
 let numbers = [7, 3, 1, 5, 8];
-let chosenBar; // Variabel for hvilken stolpe som er valgt
-let inputValue; // Variabel for hva som er skrevet i input-feltet
+// Currently selected bar graph element, if any.
+let selectedBar;
+// Current content of text input field.
+let inputValue;
 
-// View
+// -=VIEW=-
 function show() {
     let svgInnerHtml = '';
 
     for (let i = 0; i < numbers.length; i++) {
-        svgInnerHtml += createBar(numbers[i], i + 1);
+        // Create a bar for each value in numbers Array.
+        svgInnerHtml += createBar(numbers[i], i + 1, selectedBar === i + 1);
     }
 
     document.getElementById('content').innerHTML = `
                 <svg id="chart" width="500" viewBox="0 0 80 60">
                     ${svgInnerHtml}
                 </svg><br />
-                Valgt stolpe: <i>ingen</i>
+                Valgt stolpe: <i>${selectedBar ? selectedBar : "ingen"}</i>
                 <br />
                 Verdi:
                 <input type="number" min="1" max="10" oninput="inputValue = this.value" />
                 <button>Legg til stolpe</button>
-                <button disabled>Endre valgt stolpe</button><br />
-                <button disabled>Fjerne valgt stolpe</button>
+                <button ${selectedBar ? selectedBar : "disabled"}>Endre valgt stolpe</button><br />
+                <button ${selectedBar ? selectedBar : "disabled"}>Fjerne valgt stolpe</button>
             `;
 }
 
-function createBar(number, barNo) {
+function clickedBar(bar) {
+    let barNo = parseInt(bar.getAttribute("barno"));
+
+    if (barNo === selectedBar) {
+        selectedBar = null;
+    } else {
+        selectedBar = barNo;
+    }
+
+    show();
+}
+
+/**
+ * Create a statistics bar.
+ * @param {number} number ?
+ * @param {number} barNo ?
+ * @param {boolean} selected Whether the bar should be marked as selected.
+ * @returns {String} SVG RECT String of a bar.
+ */
+function createBar(number, barNo, selected = false) {
     const width = 8;
     const spacing = 2;
     let x = (barNo - 1) * (width + spacing);
@@ -33,9 +56,8 @@ function createBar(number, barNo) {
     let y = 60 - height;
     let color = calcColor(1, 10, barNo);
 
-    return `<rect width="${width}" height="${height}"
-                  x="${x}" y="${y}" fill="${color}">
-            </rect>`;
+    return `<rect class="clickable${selected === true ? " selected-item" : ""}" width="${width}" height="${height}" 
+                  x="${x}" y="${y}" fill="${color}" barno="${barNo}" onClick="clickedBar(this)"></rect>`;
 }
 
 function calcColor(min, max, val) {
@@ -46,7 +68,7 @@ function calcColor(min, max, val) {
     return colString;
 }
 
-// Controller
+// -=CONTROLLER=-
 
 
 show();
